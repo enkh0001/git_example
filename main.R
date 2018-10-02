875ab2f0440f25dc3321af4a18affdff59f96985
enkh0001 <maaraagiingirl@gmail.com>
2018-10-02 16:45:39 +0800

    Update main.R

--- main.R
+++ main.R
@@ -7,4 +7,4 @@ library(data.table)
 #request url and save response to variable titanic
 titanic<-fread("http://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv")
 # write output to table and print first 10 rows
-write.csv(titanic, file = "out/tables/titanic.csv", row.names = FALSE)
+write.csv(titanic, file = "/data/out/tables/titanic.csv", row.names = FALSE)
